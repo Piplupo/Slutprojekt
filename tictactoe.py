52061fb029f1e6f426c9dc703ab1461b1460977c
Demian <125279771+Piplupo@users.noreply.github.com>
2023-03-28 09:44:29 +0200

    Added 3x3 board

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,15 @@
 import numpy as np
+
+Board = np.zeros((3,3), int)
+
+counter = 0
+
+for i in range(0, 3):
+    for j in range(0, 3):
+        counter += 1
+        Board[i,j] = counter
+
+for i in range(3):
+    for j in range(3):
+        print([Board[i,j]], end="")
+    print()
