5ae162e606fc98f12fd6b6897c3f38b4bbb2c7e7
Demian <125279771+Piplupo@users.noreply.github.com>
2023-04-25 09:57:19 +0200

    Added random_number which i will use for my oponent bot. also added player and Oponent and used numbers. Also started working on choice for the player.

--- tictactoe.py
+++ tictactoe.py
@@ -1,8 +1,24 @@
 import numpy as np
 
+NUMBERS = (for i in range(9))
+
 Board = np.zeros((3,3), int)
 
 counter = 0
+
+USED_NUMBERS = []
+
+Player = "X"
+
+Oponent = "O"
+
+def random_number():
+    return np.random.randint(1, 9)
+def check(number):
+    if number in USED_NUMBERS:
+        return True
+    USED_NUMBERS.append(number)
+    return False
 
 for i in range(0, 3):
     for j in range(0, 3):
@@ -13,3 +29,7 @@ for i in range(3):
     for j in range(3):
         print([Board[i,j]], end="")
     print()
+    
+choice = input("Choose a number"):
+    if choice = "1":
+        np.char.replace("1", "X")
