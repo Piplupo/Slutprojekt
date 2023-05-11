8893f43ecf95bd3c641a0ef80d0ee854b05016c4
Demian <125279771+Piplupo@users.noreply.github.com>
2023-05-11 23:39:23 +0200

    Added loop and oponents turn

--- tictactoe.py
+++ tictactoe.py
@@ -1,71 +1,74 @@
 import numpy as np
+import random
 
 NUMBERS = [i + 1 for i in range(9)]
 
-Board = np.zeros((3,3), str)
+Board = np.zeros((3, 3), str)
 
 counter = 0
 
 USED_NUMBERS = []
 
 Player = "X"
-
-Oponent = "O"
+Opponent = "O"
 
 O = ("1", "2", "3", "4", "5", "6", "7", "8", "9")
 
 def oponents_turn():
-    O[random.randint(0, len(O))]
+    return random.choice(O)
 
 for i in range(0, 3):
     for j in range(0, 3):
         counter += 1
-        Board[i,j] = counter
+        Board[i, j] = counter
 
 def write():
     for i in range(3):
         for j in range(3):
-            print([Board[i,j]], end="")
+            print([Board[i, j]], end="")
         print()
-    
-choice = int(input("Choose a number: "))
-if choice == 1:
-    # return np.char.replace("1", "1X")
-    Board[0][0] = "X"
-elif choice == 2:
-    Board[0][1] = "X"
-elif choice == 3:
-    Board[0][2] = "X"
-elif choice == 4:
-    Board[1][0] = "X"
-elif choice == 5:
-    Board[1][1] = "X"
-elif choice == 6:
-    Board[1][2] = "X"
-elif choice == 7:
-    Board[2][0] = "X"
-elif choice == 8:
-    Board[2][1] = "X"
-elif choice == 9:
-    Board[2][2] = "X"
 
-oponents_turn = True
-if oponents_turn == "1":
-   Board[0][0] = "O"
-elif oponents_turn == "2":
-    Board[0][1] = "O"
-elif oponents_turn == "3":
-    Board[0][2] = "O"
-elif oponents_turn == "4":
-    Board[1][0] = "O"
-elif oponents_turn == "5":
-    Board[1][1] = "O"
-elif oponents_turn == "6":
-    Board[1][2] = "O"
-elif oponents_turn == "7":
-    Board[2][0] = "O"
-elif oponents_turn == "8":
-    Board[2][1] = "O"
-elif oponents_turn == "9":
-    Board[2][2] = "O"
-write()
+while True:
+    choice = int(input("Choose a number: "))
+    if choice == 1:
+        Board[0][0] = "X"
+    elif choice == 2:
+        Board[0][1] = "X"
+    elif choice == 3:
+        Board[0][2] = "X"
+    elif choice == 4:
+        Board[1][0] = "X"
+    elif choice == 5:
+        Board[1][1] = "X"
+    elif choice == 6:
+        Board[1][2] = "X"
+    elif choice == 7:
+        Board[2][0] = "X"
+    elif choice == 8:
+        Board[2][1] = "X"
+    elif choice == 9:
+        Board[2][2] = "X"
+
+    
+    oponents_turn_choice = oponents_turn()
+
+    if oponents_turn_choice == "1":
+        Board[0][0] = "O"
+    elif oponents_turn_choice == "2":
+        Board[0][1] = "O"
+    elif oponents_turn_choice == "3":
+        Board[0][2] = "O"
+    elif oponents_turn_choice == "4":
+        Board[1][0] = "O"
+    elif oponents_turn_choice == "5":
+        Board[1][1] = "O"
+    elif oponents_turn_choice == "6":
+        Board[1][2] = "O"
+    elif oponents_turn_choice == "7":
+        Board[2][0] = "O"
+    elif oponents_turn_choice == "8":
+        Board[2][1] = "O"
+    elif oponents_turn_choice == "9":
+        Board[2][2] = "O"
+
+    write()
